97bc9ec3a0d76d021b0937620c0bf413845b06ae
Cynthia Nikolai <cynthianikolai@gmail.com>
2022-01-01 04:11:13 -0500

    variables, data types, and data

---
+++ variables-and-data.js
@@ -0,0 +1,41 @@
+/**
+ * Variables, Data Types, and Typing
+ */
+let wordData;
+
+wordData = "Word Data";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+//6 major types - 5 primitive types (boolean, null, undefined, number (int and float and double), string) and 1 composite type (object)
+let obj = {
+    key1: "value",
+    key2: 4,
+    boolean: true,
+    obj2: {
+        obj2Key1: "Internal Object Value"
+    }
+};
+
+console.log(obj.obj2.obj2Key1)
+
+let variable = "string";
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod =  5 % 2;
+
+let string = "String 1 " + "String 2";
+
